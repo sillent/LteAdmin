8c78a159fb00fbca47914689cb2f0972222d7453
Dmitry Ulyanov <sillent1987@gmail.com>
2015-04-06 13:54:48 +0300

    сделал работу с базой через отдельный класс.

--- src/ru/megafon/lte/Add.java
+++ src/ru/megafon/lte/Add.java
@@ -1,18 +1,15 @@
 package ru.megafon.lte;
 
-import javax.naming.InitialContext;
-import javax.naming.NamingException;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.sql.DataSource;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.sql.*;
 import java.text.SimpleDateFormat;
 import java.util.*;
-import java.util.Date;
 
 
 /**
@@ -21,148 +18,111 @@ import java.util.Date;
  */
 public class Add extends HttpServlet {
     Connector connector;
-    private String Ip="";       // \
-    private String Msisdn="";   //  | -инициализируем пустыми строками чтобы работали проверки .length()
-    private String Route="";    // /
-//    private boolean exist=false;  // для проверки что уже номер заведен в БД
-    private final String SQL_CHECK_MSISDN="select * from radchecks where username=?";
-    private final String SQL_INS_MSISDN_AUTH="insert into radchecks (username,attrib,op,value) values (?,'clear',':=','password')";
-    private final String SQL_INS_IP="insert into radreplies (username,attrib,op,value,created_at,updated_at) values (?,'Framed-IP-Address',':=',?,?,?)";
-    private final String SQL_IN_ROUTE="insert into radreplies (username,attrib,op,value,created_at,updated_at) values (?,'Framed-Route','+=',?,?,?)";
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        boolean exist=false;
-        Enumeration<String> en = req.getParameterNames();
-        Connection connection;
+        boolean existInDb;
         PrintWriter pw = resp.getWriter();
-        SimpleDateFormat sdf=new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-        java.util.Date date = new java.util.Date();
-        String dateNow=sdf.format(date);
-        while (en.hasMoreElements()) {
-            String s = en.nextElement();
-            if (s.compareTo("ip") == 0) {
-                setIp(req.getParameter("ip"));
-            }
-            if (s.compareTo("route") == 0) {
-                setRoute(req.getParameter("route"));
-            }
-            if (s.compareTo("msisdn") == 0) {
-                setMsisdn(req.getParameter("msisdn"));
-            }
-        }
-        if (getMsisdn().length() > 0 && (getIp().length() > 0 || getRoute().length() > 0)) {
-            // Проверяем что номер не заведен в БД
-            try {
-                InitialContext ctx = new InitialContext();
-                DataSource ds = (DataSource) ctx.lookup("jdbc/myresource");
-                connection=ds.getConnection();
-                try {
-                    PreparedStatement stmt = connection.prepareStatement(SQL_CHECK_MSISDN);
-                    stmt.setString(1, getMsisdn());
-                    ResultSet resultSet = stmt.executeQuery();
-                    if (resultSet.next()) {
-                        exist = true;
-                    }
-                    stmt.close();
-                } catch (SQLException e) {
-                    e.printStackTrace();
-                }
+        // Connection initization
+        connector = new Connector();
 
-                if (exist && getRoute().length() == 0) {    // если обычное добавление связки MSISDN-IP при существующей связке
+
+        try {
+            existInDb = connector.check(req.getParameter("msisdn"));      //проверяем присутствие номера в БД
+            int type = doCheck(req);                  // 1 - msisdn+ip
+            // 2 - msisdn+ip+route
+            // 3 - msisdn+route
+            // 0 - other
+            if (type == 0) {
+                printResponse(pw, 600, null);
+                return;
+            }
+
+            if (type == 1) {
+                String msisdn = req.getParameter("msisdn");
+                String ip = req.getParameter("ip");
+                if (existInDb) {
                     printResponse(pw, 600, null);
-                    connection.close();
                     return;
                 }
-                if (exist && getRoute().length() > 0 && getIp().length() > 0) {  // msisdn=<yes>&ip=&route=
-                    try {
-                        PreparedStatement stmt = connection.prepareStatement(SQL_CHECK_MSISDN);
-                        stmt.setString(1, getMsisdn());
-                        ResultSet resultSet = stmt.executeQuery();
-                        if (resultSet.next()) {
-                            String msisdnE = resultSet.getString(2);
-                            printResponse(pw, 600, msisdnE);
-                        }
-                        stmt.close();
-                        connection.close();
-                        return;
-                    } catch (SQLException e) {
-                        e.printStackTrace();
-                    }
-                }
-                if (!exist && getRoute().length() == 0 && getIp().length() > 0) {  // msisdn=<no>&ip= - добавить в обе таблицы
-                    PreparedStatement stmtauth = connection.prepareStatement(SQL_INS_MSISDN_AUTH);
-                    PreparedStatement stmt = connection.prepareStatement(SQL_INS_IP);
-                    stmtauth.setString(1,getMsisdn());
-                    stmt.setString(1, getMsisdn());
-                    stmt.setString(2, getIp());
-                    stmt.setString(3, dateNow);
-                    stmt.setString(4, dateNow);
-                    stmtauth.executeUpdate();
-                    stmt.executeUpdate();
-                    stmtauth.close();
-                    stmt.close();
-                    connection.close();
-                    printResponse(pw,200,null);
+                if (!existInDb) {
+                    connector.insertMsisdnInChecks(msisdn);
+                    connector.insertIpInReplies(msisdn, ip);
+                    printResponse(pw, 200, null);
                     return;
                 }
-                if (exist && getRoute().length() > 0 && getIp().length() == 0) {   // msisdn=<yes>&route= - добавить
-                    PreparedStatement stmtRoute = connection.prepareStatement(SQL_IN_ROUTE);
-                    stmtRoute.setString(1, getMsisdn());
-                    stmtRoute.setString(2, getRoute());
-                    stmtRoute.setString(3, dateNow);
-                    stmtRoute.setString(4, dateNow);
-                    stmtRoute.executeUpdate();
-                    stmtRoute.close();
-                    connection.close();
-                    printResponse(pw,200,null);
-                    return;
-                }
-                if (!exist && getRoute().length() > 0 && getIp().length() == 0) {
-                    printResponse(pw,600,null);
-                }
-            } catch (NamingException ne) {
-                ne.printStackTrace();
-            } catch (SQLException sqle) {
-                printResponse(pw,666,null);
             }
+            if (type == 2) {
+                String msisdn = req.getParameter("msisdn");
+                String ip = req.getParameter("ip");
+                String route = req.getParameter("route");
+
+                if (existInDb) {
+                    printResponse(pw, 600, null);
+                    return;
+                }
+                if (!existInDb) {
+                    connector.insertMsisdnInChecks(msisdn);
+                    connector.insertIpInReplies(msisdn, ip);
+                    connector.insertRouteInReplies(msisdn, route);
+                    printResponse(pw, 200, null);
+                    return;
+                }
+            }
+            if (type == 3) {
+                String msisdn = req.getParameter("msisdn");
+                String route = req.getParameter("route");
+                if (!existInDb) {
+                    printResponse(pw, 600, null);
+                    return;
+                }
+                if (existInDb) {
+                    connector.insertRouteInReplies(msisdn, route);
+                    printResponse(pw, 200, null);
+                    return;
+                }
+
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
         }
     }
 
-    private Connection initConnection() {
-        connector=new Connector();
-        Connection connection = connector.getConnection();
-        if (connection == null) {
-            return null;
+    public int doCheck(HttpServletRequest req) {
+        boolean msisdn = false, ip = false, route = false;
+        Enumeration<String> paramNameEnum = req.getParameterNames();
+        while (paramNameEnum.hasMoreElements()) {
+            String pname = paramNameEnum.nextElement();
+            if (pname.equals("msisdn")) {
+                msisdn = true;
+                System.out.println("msisdn set to true");
+            }
+            if (pname.equals("ip")) {
+                ip = true;
+                System.out.println("ip set to true");
+            }
+            if (pname.equals("route")) {
+                route = true;
+                System.out.println("route set to true");
+            }
+            System.out.println("pname = "+pname);
         }
-        return connection;
-    }
-    private void closeConnection() {
-        connector.closeConnection();
-    }
-
-    public String getIp() {
-        return Ip;
-    }
-
-    public void setIp(String ip) {
-        Ip = ip;
-    }
-
-    public String getMsisdn() {
-        return Msisdn;
-    }
-
-    public void setMsisdn(String msisdn) {
-        Msisdn = msisdn;
-    }
-
-    public String getRoute() {
-        return Route;
-    }
-
-    public void setRoute(String route) {
-        Route = route;
+        if (msisdn && !ip && !route) {
+            System.out.println("return 0");
+            return 0;
+        } else if (msisdn && ip && !route) {
+            System.out.println("return 1");
+            return 1;
+        } else if (msisdn && ip && route) {
+            System.out.println("return 2");
+            return 2;
+        } else if (msisdn && !ip && route) {
+            System.out.println("return 3");
+            return 3;
+        } else {
+            System.out.println("return else");
+            return 0;
+        }
     }
 
     public void printResponse(PrintWriter pw, int code, String msisdn_exist) {
@@ -173,6 +133,5 @@ public class Add extends HttpServlet {
             pw.println("<msisdn>" + msisdn_exist + "</msisdn>");
         }
         pw.println("</result>");
-
     }
 }
--- src/ru/megafon/lte/Connector.java
+++ src/ru/megafon/lte/Connector.java
@@ -1,64 +1,143 @@
 package ru.megafon.lte;
 
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
 import java.sql.Connection;
-import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
 import java.text.SimpleDateFormat;
-import java.util.Date;
+import java.util.*;
 
 /**
  * Created by santa on 30.03.15.
  */
+
+
 public class Connector {
-
-    Connection connection;
-    private static String driver = "com.mysql.jdbc.Driver";
-    private static String jdbc="jdbc:mysql";
-    private static String hostName = "127.0.0.1";
-    private static String osid = "radius";
-    private static String nam = "root";
-    private static String pas = "root";
-    private static int portNo = 3306;
-    boolean connected=false;
-
-    public void setConnected(boolean connected) {
-        this.connected = connected;
+    public enum Type {
+        MSAUTH,   // MSISDN into radchecks
+        IPREPL,   // MSISDN&IP into radreplies
+        ROUTREPL  // MSISDN&ROUTE into radreplies
     }
 
-    public boolean isConnected() {
-        return connected;
-    }
+    private  String SQL_CHECK_MSISDN="select * from radchecks where username=?";
+    private  String SQL_INS_MSISDN_AUTH="insert into radchecks (username,attrib,op,value) values (?,'clear',':=','password')";
+    private  String SQL_INS_IP="insert into radreplies (username,attrib,op,value,created_at,updated_at) values (?,'Framed-IP-Address',':=',?,?,?)";
+    private  String SQL_INS_ROUTE="insert into radreplies (username,attrib,op,value,created_at,updated_at) values (?,'Framed-Route','+=',?,?,?)";
+    private Connection connection;
 
+    HashMap<String, String> map;
     Connector() {
+        this("jdbc/myresource");
+    }
+
+    Connector(String name) {
         try {
-            Class.forName(driver);
-        } catch (ClassNotFoundException e) { e.printStackTrace(); }
-        try {
-            if (!isConnected()) {
-                connection = DriverManager.getConnection(jdbc + "://" + hostName+":"+portNo+"/" + osid, nam, pas);
-                setConnected(true);
-            }
+            map = new HashMap<String, String>();
+            InitialContext ctx = new InitialContext();
+            DataSource dts = (DataSource) ctx.lookup(name);
+            connection = dts.getConnection();
+        } catch (NamingException nexcp) {
+            nexcp.printStackTrace();
         } catch (SQLException e) {
             e.printStackTrace();
-            setConnected(false);
         }
     }
 
     public Connection getConnection() {
-        if(isConnected()) {
-            return connection;
-        } else
-            return null;
+        return connection;
     }
-    // @close connection
-    public void closeConnection() {
+
+    private void closeConnection(Connection conn) {
         try {
-            if (isConnected()) {
-                connection.close();
-            }
+            conn.close();
         } catch (SQLException e) {
-            System.err.println("cannot close connection");
+            e.printStackTrace();
+        }
+    }
+
+    public boolean check(String msisdn) throws SQLException {
+        if (msisdn.length()<=0) {
+            return false;
+        }
+        PreparedStatement stmt = connection.prepareStatement(SQL_CHECK_MSISDN);
+        stmt.setString(1, msisdn);
+        ResultSet resultSet=stmt.executeQuery();
+        if (resultSet.next()) {
+            stmt.close();
+            return true;
+        } else {
+            stmt.close();
+            return false;
+        }
+    }
+    public void insertMsisdnInChecks(String msisdn) throws SQLException {
+        map.put("msisdn", msisdn);
+        System.out.println("map msisdn=" + map.get("msisdn"));
+        insert(map, Type.MSAUTH);
+    }
+
+    public void insertIpInReplies(String msisdn, String ip) throws SQLException {
+        map.put("msisdn", msisdn);
+        map.put("ip", ip);
+        insert(map, Type.IPREPL);
+    }
+    public void insertRouteInReplies(String msisdn, String route) throws SQLException {
+
+        map.put("msisdn",msisdn);
+        map.put("route", route);
+        insert(map,Type.ROUTREPL);
+    }
+
+    public void insert(HashMap<String, String> arg, Type type) {
+        String msisdn=arg.get("msisdn");
+        SimpleDateFormat smpdf=new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        Date now = new Date();
+        String timeNow=smpdf.format(now);
+        PreparedStatement stmt;
+        if (type == Type.MSAUTH) {
+            try {
+                System.out.println("execute insert MSAUTH");
+                stmt = connection.prepareStatement(SQL_INS_MSISDN_AUTH);
+                stmt.setString(1, msisdn);
+                System.out.println(stmt.toString());
+                stmt.executeUpdate();
+                stmt.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+            return;
+        }
+        if (type == Type.IPREPL) {
+            String ip = arg.get("ip");
+            try {
+                stmt = connection.prepareStatement(SQL_INS_IP);
+                stmt.setString(1, msisdn);
+                stmt.setString(2, ip);
+                stmt.setString(3, timeNow);
+                stmt.setString(4, timeNow);
+                stmt.executeUpdate();
+                stmt.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+            return;
+        }
+        if (type == Type.ROUTREPL) {
+            String route = arg.get("route");
+            try {
+                stmt = connection.prepareStatement(SQL_INS_ROUTE);
+                stmt.setString(1, msisdn);
+                stmt.setString(2, route);
+                stmt.setString(3, timeNow);
+                stmt.setString(4, timeNow);
+                stmt.executeUpdate();
+                stmt.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
         }
     }
 }
